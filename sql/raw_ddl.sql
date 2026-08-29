0ba8ca5bbe8525b0f0594af0d3f9453470aa208f
silvallana <roxannesilvallana@gmail.com>
2026-08-29 16:28:33 +0800

    Add raw DDL

---
+++ sql/raw_ddl.sql
@@ -0,0 +1,45 @@
+CREATE SCHEMA IF NOT EXISTS raw;
+
+CREATE TABLE IF NOT EXISTS raw.themes (
+    id TEXT, name TEXT, parent_id TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.colors (
+    id TEXT, name TEXT, rgb TEXT, is_trans TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.part_categories (
+    id TEXT, name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.part_relationships (
+    rel_type TEXT, child_part_num TEXT, parent_part_num TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.elements (
+    element_id TEXT, part_num TEXT, color_id TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.sets (
+    set_num TEXT, name TEXT, year TEXT, theme_id TEXT, num_parts TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.minifigs (
+    fig_num TEXT, name TEXT, num_parts TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.inventories (
+    id TEXT, version TEXT, set_num TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.inventory_parts (
+    inventory_id TEXT, part_num TEXT, color_id TEXT, quantity TEXT, is_spare TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.inventory_sets (
+    inventory_id TEXT, set_num TEXT, quantity TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.inventory_minifigs (
+    inventory_id TEXT, fig_num TEXT, quantity TEXT
+);
